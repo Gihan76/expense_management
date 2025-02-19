5fd1f6e1297dd361e5ff7c7930c0c783ad7380f0
Gihan Wijesinghe <gihantharaka76@gmail.com>
2025-02-19 08:33:39 +0530

    responsiveness improved

--- src/App.jsx
+++ src/App.jsx
@@ -20,14 +20,14 @@ function App() {
     <>
       <Grid2 container spacing={2} sx={{ padding: 2 }}>
         {/* form */}
-        <Grid2 size={4}>
+        <Grid2 size={{xs: 12, sm: 12, md: 4}}>
           <Paper elevation={3} sx={{ padding: 2 }}>
             <AddExpenseForm />
           </Paper>
         </Grid2>
 
         {/* table */}
-        <Grid2 size={8}>
+        <Grid2 size={{xs: 12, sm: 12, md: 8}} >
           <Paper elevation={3} sx={{ padding: 2 }}>
             <ExpensesTable/>
           </Paper>
